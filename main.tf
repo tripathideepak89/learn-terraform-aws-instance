07d348306d32f86bbb4d732cd2f663525d4f8f0a
Deepak Tripathi <tripathideepak2489@gmail.com>
2024-05-31 15:39:09 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_instance" "vm1" {
+  ami           = "ami-0440d3b780d96b29d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name1
+  }
+}
+
+
+resource "aws_instance" "vm2" {
+  ami           = "ami-0440d3b780d96b29d"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.my_subnet.id
+
+  tags = {
+    Name = var.instance_name2
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "deepakbucket2024053101"
+
+  tags = {
+    Name = var.s3_bucket_name
+  }
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "GameScores"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+  attribute {
+    name = "TopScore"
+    type = "N"
+  }
+
+
+  global_secondary_index {
+    name               = "GameTitleIndex"
+    hash_key           = "GameTitle"
+    range_key          = "TopScore"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["UserId"]
+  }
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "production"
+  }
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "instance_name1" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "ExampleAppServerInstance1"
+}
+
+variable "instance_name2" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "ExampleAppServerInstance2"
+}
+
+variable "manual_instance_name" {
+  description = "Value of the Name tag for the manually created EC2 instance"
+  type        = string
+  default     = "manually_created_vm"
+}
+
+variable "s3_bucket_name" {
+  description = "value of the name tag for the s3 bucket"
+  type        = string
+  default     = "bucket1"
+
+}
+
+variable "dynamodb_name" {
+  description = "value of the name tag for the dynamo db"
+  type        = string
+  default     = "db"
+
+}
